77142ac3b4ab50231ca9fd51ff183a2f488db74b
[yangjialu@52tt.com] <[573669898@qq.com]>
2020-11-18 09:31:15 +0800

    first commit

---
+++ src/test/java/com/excel/Demo.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+public class Demo {
+    public static void main(String[] args) {
+        // java读取excel内容，打印在控制台上
+        /*
+        * 1、找到excel的位置
+        * 2、双击打开
+        * 3、找到sheet
+        *
+        * */
+    }
+}
