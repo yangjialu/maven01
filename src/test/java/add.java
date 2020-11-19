d2d4b05add8824a4a2f94e62eb9aa0b4c1443fdb
[yangjialu@52tt.com] <[573669898@qq.com]>
2020-11-19 16:21:46 +0800

    add

---
+++ src/test/java/add.java
@@ -0,0 +1,21 @@
+public class add {
+    public static void main(String[] args) {
+        // 输出101到200内的质数
+        int count =0;
+        for (int i=101; i<200; i++) {
+            boolean f = true;
+            for (int j=2; j<i; j++) {
+                if (i % j == 0) {
+                    f = false;
+                    break;
+                }
+            }
+            if (f) {
+                count++;
+                System.out.print(" " + i);
+            }
+        }
+        System.out.println();
+        System.out.println("一共有" + count + "个质数");
+    }
+}
