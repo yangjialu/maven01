10b5f787dc334df4d19b6bd66aae01cefe893a16
[yangjialu@52tt.com] <[573669898@qq.com]>
2020-11-19 15:01:40 +0800

    add 读写excel方法

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,17 +1,19 @@
 public class Demo {
     public static void main(String[] args) {
-        int i = 20; int j = 13;
+        int i = 20; int j = 18;
         int i1 = i++;
         System.out.println("i = " + i);
         System.out.println("i1 = " + i1);
 
         int i2 = ++i;
         System.out.println("i2 = " + i2);
+        System.out.println("i = " + i);
         int j2 = --j;
         System.out.println("j = " + j);
         System.out.println("j2 = " + j2);
         int j3 = j--;
         System.out.println("j3 = " + j3);
         System.out.println("j = " + j);
+
     }
 }
