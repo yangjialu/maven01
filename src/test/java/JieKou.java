ce6af71be07735954f37515c9aebae9d1991d1d1
yangjialu <573669898@qq.com>
2020-11-29 13:51:12 +0800

    add interface

---
+++ src/test/java/JieKou.java
@@ -0,0 +1,10 @@
+public interface JieKou {
+    // 接口中申明的属性默认是public static final，也只能是public static final
+    public static final int id = 1;
+
+    // 接口中只能定义抽象方法，而且这些方法默认是public的，也只能是public
+    // 接口可以继承其他的接口，并添加新的属性和抽象方法
+    public void start();
+    public void run();
+    public void stop();
+}
---
+++ src/test/java/JieKou2.java
@@ -0,0 +1,12 @@
+public class JieKou2 implements JieKou1 {
+    @Override
+    public void sing() {
+        System.out.println("家禄唱歌");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("家禄睡觉");
+    }
+
+}
