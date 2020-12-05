b695baf2d8e6e1181f5477bdefeec793e94360bd
yangjialu <yangjialu@52tt.com>
2020-12-05 14:55:27 +0800

    add

--- src/test/java/TestEquals.java
+++ src/test/java/TestEquals.java
@@ -3,6 +3,10 @@ public class TestEquals {
         Cat c1 = new Cat(1, 2, 3);
         Cat c2 = new Cat(1, 2, 3);
         System.out.println(c1.equals(c2));
+        String s1 = new String("hello");
+        String s2 = new String("hello");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
     }
 }
 
@@ -20,7 +24,8 @@ class Cat {
     public boolean equals(Object obj) {
         if (obj == null) {
             return false;
-        } else {
+        }
+        else {
             if (obj instanceof Cat) {
                Cat c = (Cat)obj;
                if(c.color == this.color && c.height== this.height && c.weight == this.weight) {
