c6c010a1fbcb25b3d0adef36e43d9f222f581fc6
yangjialu <yangjialu@52tt.com>
2020-12-05 14:49:24 +0800

    add

--- src/test/java/TestEquals.java
+++ src/test/java/TestEquals.java
@@ -1,7 +1,7 @@
 public class TestEquals {
     public static void main(String[] args) {
         Cat c1 = new Cat(1, 2, 3);
-        Cat c2 = new Cat(2, 2, 3);
+        Cat c2 = new Cat(1, 2, 3);
         System.out.println(c1.equals(c2));
     }
 }
