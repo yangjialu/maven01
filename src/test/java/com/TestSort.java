48582321975ed2d3d0c4fbf75913f9ba101af7f5
yangjialu <yangjialu@52tt.com>
2020-12-18 09:26:56 +0800

    add

--- src/test/java/com/TestSort.java
+++ src/test/java/com/TestSort.java
@@ -5,9 +5,12 @@ public class TestSort {
         int[] a = {1, 3, 2, 7, 9, 8, 4, 6, 5};
         selectSort(a);
         print(a);
+        bubbleSort(a);
+        print(a);
     }
 
-    static void selectSort(int[] a) {
+    // 选择排序
+    private static void selectSort(int[] a) {
         int k, temp;
         for (int i = 0; i < a.length; i++){
             k = i;
@@ -23,10 +26,25 @@ public class TestSort {
             }
         }
     }
+    // 冒泡排序
+    private static void bubbleSort(int[] a) {
+        int temp;
+        for (int i = a.length-1; i >= 1 ; i--) {
+            for (int j = 0; j <=i-1; j++) {
+                if(a[j] > a[j+1]) {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+    }
 
     private static void print(int[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
+        System.out.println();
     }
 }
+
