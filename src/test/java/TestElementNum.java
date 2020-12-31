a95f7f4f15ee106fdde0d117a6d4a895d60d1122
yangjialu <yangjialu@52tt.com>
2020-12-31 18:57:05 +0800

    添加

---
+++ src/test/java/TestElementNum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class TestElementNum {
+    public static void main(String[] args) {
+        String s = "asdfdbfufblkseeereiojiklmnlnncnjdkdndjddnl";
+        Map m = new HashMap();
+
+        /*
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            m.put(c, m.get(c) == null? 1: (Integer)m.get(c) + 1);
+        }
+        System.out.println(m);
+        System.out.println(m.get('d'));
+        System.out.println(m.keySet());
+        System.out.println(m.values());
+        System.out.println(m.entrySet());
+         */
+        String[] arr = s.split("");
+        for (int i = 0; i < arr.length; i++) {
+            m.put(arr[i], m.get(arr[i]) == null? 1:(Integer)m.get(arr[i]) + 1);
+        }
+        System.out.println(m);
+        System.out.println(m.get("a"));
+    }
+}
