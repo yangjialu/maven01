a523178ba6dba08328f0721ee8469accfd05a526
yangjialu <yangjialu@52tt.com>
2021-01-04 16:39:02 +0800

    添加1

---
+++ src/test/java/TestObjectStream.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class TestObjectStream {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        T t = new T();
+        t.k = 8;
+        FileOutputStream fos = new FileOutputStream(System.getProperty("user.dir") + "\\date1.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(t);
+        oos.flush();
+        oos.close();
+
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\date1.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        T tRead = (T)ois.readObject();
+        System.out.println(tRead.i + " " + tRead.j + " " + tRead.d + " " + tRead.k);
+    }
+}
+
+
+class T implements Serializable {
+    int i = 10;
+    int j = 9;
+    double d = 2.3;
+    transient int k = 15;  // transient修饰的不考虑值
+}
