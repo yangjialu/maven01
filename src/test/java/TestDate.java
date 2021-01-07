fe17bf743cb04f3134267588716235b73e22035a
yangjialu <yangjialu@52tt.com>
2021-01-07 09:35:04 +0800

    添加Thread  生产者与消费者

---
+++ src/test/java/TestDate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.text.*;
+
+public class TestDate {
+    public static void main(String[] args) {
+        Date dNow = new Date( );
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+
+        System.out.println("当前时间为: " + ft.format(dNow));
+    }
+}
